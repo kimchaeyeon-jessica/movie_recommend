82ae4a9228fe77a06fb8588377072a3ae2f4ed70
kimchaeyeon <jessicakcy0501@gmail.com>
2020-12-04 20:22:38 +0900

    좋아하는 영화 입력 받아 딕셔너리로 저장

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -11,7 +11,7 @@ def main():
     popularity = data.pop("popularity") #인기도
     title = data.pop("title") #제목
     vote_aver = data.pop("vote_average") #평균 평점
-    vote_count = data #평점 갯수
+    vote_count = data #평점 개수
 
     print(title)
 
@@ -19,13 +19,12 @@ def main():
         genres[i] = modify_data(genres[i])
         keywords[i] = modify_data(keywords[i])
     
-    #write your code here... ok ok~
     favorite_movies = []
-    for i in range(2): #나중에 입력 개수 바꾸기!
+    for i in range(3): #나중에 입력 개수 바꾸기!
         favorite_movies.append(input("좋아하는 영화를 입력하세요:"))
-    gen_dict, kw_dict, len_dict = create_BOW(favorite_movies, title.tolist(), [genres.tolist(),keywords.tolist(),ori_lan.tolist()])
+    gen_dict, kw_dict, lan_dict = create_BOW(favorite_movies, title.tolist(), [genres.tolist(),keywords.tolist(),ori_lan.tolist()])
     
-    print(gen_dict, kw_dict, len_dict) #확인용
+    print(gen_dict, kw_dict, lan_dict) #확인용
     
 def modify_data(list_df):
     list_df = list_df.split(",")
@@ -37,25 +36,34 @@ def modify_data(list_df):
     return new_df
 
 def create_BOW(movies, title, features):
-    gen_dict, kw_dict, len_dict = {}, {}, {}
+    gen_dict, kw_dict, lan_dict = {}, {}, {}
     
     for m in movies:
         mindex = title.index(m)
         genre = features[0][mindex]
-        #keyword, len 정의
-        
+        keyword = features[1][mindex]
+        lan = features[2][mindex]
+
         for gen in genre:
             if gen not in gen_dict:
                 gen_dict[gen] = 1
             else :
                 gen_dict[gen] += 1
-        #위의 gen_dict처럼 kw_dict생성
-        """if leng not in len_dict:
-            len_dict[leng] = 1
-        else :
-            len_dict[leng] += 1"""
+                
+        for kw in keyword:
+            if kw not in kw_dict:
+                kw_dict[kw] = 1
+            else :
+                kw_dict[kw] += 1 
+                
+        for lang in lan:
+            if lang not in lan_dict:
+                lan_dict[lang] = 1
+            else :
+                lan_dict[lang] += 1
+            
         
-    return gen_dict, kw_dict, len_dict
+    return gen_dict, kw_dict, lan_dict
 
 if __name__ == "__main__":
     main()
